6e6fa07fd1fd4f3dd59920588a4026d369c2b613
Harald Kjølberg <harald.kjolberg@zenitel.com>
2022-10-13 11:06:49 +0200

    Disabled the 'Try it out' feature as this has little value on Github.

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -6,6 +6,7 @@ window.onload = function() {
     url: "openapi/openapi.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
+    supportedSubmitMethods: [],
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
