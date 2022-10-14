9603462fea4a485562f8ba886169409b07ebf865
Harald Kjølberg <harald.kjolberg@zenitel.com>
2022-10-14 10:58:52 +0200

    Updated URL for Swagger UI

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "openapi/openapi.yaml",
+    url: "https://zenitel-as.github.io/Zenitel-Link/openapi/openapi.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
